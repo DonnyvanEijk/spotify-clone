50e7f61550ee6498ec2a0d1d04e9c4b662b7c0ff
Donny van Eijk <donnyvaneijk873@gmail.com>
2024-12-18 15:42:15 +0100

    New shuffle method YAY

--- app/(site)/playlist/[playlistId]/components/Controls.tsx
+++ app/(site)/playlist/[playlistId]/components/Controls.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import ShuffleButton from "@/components/ShuffleButton";
 import PlaylistPopover from "@/components/PlaylistPopover";
 import { Song } from "@/types";
@@ -13,16 +13,10 @@ interface ShuffleControlProps {
 }
 
 const ShuffleControl: React.FC<ShuffleControlProps> = ({ songs, isOwner, id }) => {
-    const [shuffle, setShuffle] = useState<boolean>(false);
-
-    const handleShuffle = () => {
-        setShuffle(!shuffle);
-    }
-
     return (
         <div className="flex flex-row gap-x-3 ml-5 w-full items-center">
             <UsablePlayButton songs={songs} />
-            <ShuffleButton doShuffle={shuffle} onShuffle={handleShuffle} size={30} />
+            <ShuffleButton size={30} />
             {isOwner && (
                 <PlaylistPopover playlistId={id} />
             )}
--- hooks/usePlayer.ts
+++ hooks/usePlayer.ts
@@ -5,10 +5,13 @@ interface PlayerStore {
   activeId?: string;
   volume: number;
   loop: boolean;
+  shuffle: boolean;
+  toggleShuffle: () => void;
   setId: (id: string) => void;
   setIds: (ids: string[]) => void;
   setVolume: (volume: number) => void;
   reset: () => void;
+  
 }
 
 const usePlayer = create<PlayerStore>((set) => ({
@@ -16,6 +19,8 @@ const usePlayer = create<PlayerStore>((set) => ({
   activeId: undefined,
   volume: 1,
   loop: false,
+  shuffle: false,
+  toggleShuffle: () => set((state) => ({ shuffle: !state.shuffle })),
   setId: (id: string) => set({ activeId: id }),
   setIds: (ids: string[]) => set({ ids }),
   setVolume: (volume: number) => set({ volume }),
