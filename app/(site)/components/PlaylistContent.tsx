0181bd2e1d39070352e972076fd0a387dd031ef2
Donny van Eijk <donnyvaneijk873@gmail.com>
2024-12-19 15:02:17 +0100

    ui bug

--- app/(site)/components/PlaylistContent.tsx
+++ app/(site)/components/PlaylistContent.tsx
@@ -8,7 +8,15 @@ interface PlaylistContentProps {
 
 const PlaylistContent: React.FC<PlaylistContentProps> = ({ playlists }) => {
     return (
-        <div className="grid grid-rows-1 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 lx:grid-cols-5 2xl:grid-cols-8 gap-4 ml-4">
+        <div className="grid 
+          grid-cols-2 
+          sm:grid-cols-3 
+          md:grid-cols-3 
+          lg:grid-cols-4 
+          xl:grid-cols-5 
+          2xl:grid-cols-8 
+          gap-4 
+          mt-4">
             {playlists.slice(0, 16).map((item) => (
                 <PlaylistItem
                     key={item.id}
