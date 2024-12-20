a10d61130b03306eece542f27adfae1b5c3db942
Donny van Eijk <donnyvaneijk873@gmail.com>
2024-12-20 09:41:50 +0100

    Reworked methods for lyrics fetch

---
+++ app/(site)/lyrics/[songId]/page.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    params: {
+        songId: string;
+    };
+}
+
+
+const LyricsPage = async ({params}:Props) => {
+    
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default LyricsPage;
