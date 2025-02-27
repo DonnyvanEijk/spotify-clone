fd9efa4040e1cb20cc5051a8cbbd24edc85ae330
Donny van Eijk <donnyvaneijk873@gmail.com>
2025-02-27 14:12:34 +0100

    new badge added

--- components/songitem.tsx
+++ components/songitem.tsx
@@ -7,6 +7,8 @@ import PlayButton from './playbutton';
 import { twMerge } from 'tailwind-merge';
 import * as ContextMenu from "@radix-ui/react-context-menu"
 import SongRightClickContent from './right_click/SongRightClickContent';
+import { differenceInHours } from 'date-fns';
+
 interface SongItemProps {
   data: Song;
   onClick: (id: string) => void;
@@ -16,6 +18,7 @@ interface SongItemProps {
 
 const SongItem: React.FC<SongItemProps> = ({ data, onClick, reactive, isOwner}) => {
   const imagePath = useLoadImage(data);
+  const isNew = differenceInHours(new Date(), new Date(data.created_at)) <= 24;
 
   return (
     <ContextMenu.Root modal={false}>
@@ -55,6 +58,16 @@ const SongItem: React.FC<SongItemProps> = ({ data, onClick, reactive, isOwner}) 
           fill
           alt='Image'
         />
+        {isNew && (
+          <div className='absolute top-2 right-2'>
+            <Image
+              src='/images/new_badge.png'
+              width={50}
+              height={50}
+              alt='New Badge'
+            />
+          </div>
+        )}
       </div>
       <div className='flex flex-col items-start w-full pt-4 gap-y-1'>
         <p className={twMerge(`font-semibold truncate w-full`, reactive && "text-purple-500 font-semibold")}>{data.title}</p>
