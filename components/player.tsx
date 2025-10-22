43f8fbda1ab8cf9177bb9159e5bff6ba4b969016
DonnyvanEijk <donnyvaneijk873@gmail.com>
2025-10-22 13:25:14 +0200

    Styling bug big screens

--- components/player.tsx
+++ components/player.tsx
@@ -14,7 +14,7 @@ const Player = () => {
   if (!song || !songUrl || !player.activeId) return null;
 
   return (
-    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 w-[95%] max-w-6xl h-[90px] bg-white/10 backdrop-blur-md rounded-3xl shadow-lg border border-white/20 px-5 py-2 z-50">
+    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 w-[95vw] max-w-9xl h-[90px] bg-white/10 backdrop-blur-md rounded-3xl shadow-lg border border-white/20 px-5 py-2 z-50">
       <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
